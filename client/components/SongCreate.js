0c7d2de6a2b229bd4f3f020cad5d8c8db66850b2
sayedpritom999 <sayedpritom999@gmail.com>
2023-02-23 00:20:28 +0600

    added fetchSong query

--- client/components/SongCreate.js
+++ client/components/SongCreate.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
 import { Link, hashHistory } from 'react-router';
-import query from '../components/queries/fetchSongs'
+import query from '../queries/fetchSongs'
 
 
 class SongCreate extends Component {
